b7d2695e8817aa63f724ff2199d9cee5725e578d
Kovalevichs <Kovalevich.Sofiya@list.ru>
2025-05-30 09:09:32 +0300

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,9 @@
+class employer:
+  first_name = 'sofia'
+  last_name = 'kovalevich'
+
+  def show(self):
+    return self.name
+employer = employer()
+print(employer.name)
+print(employer.surname)
---
+++ 9.py
@@ -0,0 +1,9 @@
+class Student:
+  first_name = 'sofia'
+  last_name = 'kovalevich'
+
+  def show(self):
+    return self.name
+Student = Student()
+print(Student.name)
+print(Student.surname)
